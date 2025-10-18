f617bbc9c4286ac77fadeb14fbbb747c46e3f26b
abhay-apm <abhaypratapmaurya.apm@gmail.com>
2025-10-19 01:13:48 +0530

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -4,7 +4,10 @@ module.exports = () => ({
   upload: {
     config: {
       provider: 'local',
-      providerOptions: { sizeLimit: 10000000 },
+      providerOptions: {
+        sizeLimit: 10000000, // 10 MB
+      },
+      // IMPORTANT: this must be under `config`, not top-level
       uploadPath: path.join('/tmp', 'uploads'),
     },
   },
